f3ddc69f64027e09c7e6a3423b0dd1fed514cb62
Muhammad Abdullah <51495095+Abdullahkiet@users.noreply.github.com>
2020-06-25 21:40:28 +0500

    Update minimax.js

--- minimax.js
+++ minimax.js
@@ -2,7 +2,7 @@
 // The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
 // https://youtu.be/I64-UTORVfU
-// https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
+
 
 function bestMove() {
   // AI to make its turn
